e7bc98e9e7587a8ae5bb84fcfb6d73c03f4f0f54
anujssmishra@gmail.com <anujssmishra@gmail.com>
2019-10-02 16:10:45 +0530

    Login button updated

--- app/src/main/java/com/example/temp/MainActivity.kt
+++ app/src/main/java/com/example/temp/MainActivity.kt
@@ -2,9 +2,21 @@ package com.example.temp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.registration.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+//import androidx.core.app.ComponentActivity
+//import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,8 +32,14 @@ class MainActivity : AppCompatActivity() {
             showHome()
         }
 
+        btn_next.setOnClickListener() {
+            val mob: String = (findViewById(R.id.mobileNumber) as EditText).text.toString()
+            val pass: String = (findViewById(R.id.password) as EditText).text.toString()
+            if ((mob.trim().length<=0) or (pass.trim().length<=0))
+                Toast.makeText(applicationContext, "Username or Password not specified!", Toast.LENGTH_SHORT).show()
+        }
 
-    }
+        }
 
     private fun showRegistration(){
         homeLayout.visibility=View.GONE
