f6e2a80f70cf73c8c95f2182b684939cc4c73782
anujssmishra@gmail.com <anujssmishra@gmail.com>
2019-11-29 21:37:23 +0530

    Linked all the layouts

--- app/src/main/java/com/example/temp/OTPActivity.kt
+++ app/src/main/java/com/example/temp/OTPActivity.kt
@@ -1,12 +1,19 @@
 package com.example.temp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_otp.*
 
 class OTPActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
+
+        confirmation.setOnClickListener {
+            val intent = Intent(this, QualificationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/temp/QualificationActivity.kt
+++ app/src/main/java/com/example/temp/QualificationActivity.kt
@@ -1,12 +1,19 @@
 package com.example.temp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_qualification.*
 
 class QualificationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qualification)
+
+        qualNext.setOnClickListener {
+            val intent = Intent(this, PreferenceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
