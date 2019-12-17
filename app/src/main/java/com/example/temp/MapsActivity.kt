e9df03f15bb1c1cf8f11439983d1873ee5d1fc81
anujssmishra <anujssmishra@gmail.com>
2019-12-17 13:26:11 +0530

    Added top bar in Maps.

--- app/src/main/java/com/example/temp/MapsActivity.kt
+++ app/src/main/java/com/example/temp/MapsActivity.kt
@@ -1,7 +1,8 @@
 package com.example.temp
 
-//import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 
@@ -11,6 +12,11 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import kotlinx.android.synthetic.main.activity_maps.btn1
+import kotlinx.android.synthetic.main.activity_maps.btn2
+import kotlinx.android.synthetic.main.activity_maps.btn3
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
@@ -23,6 +29,37 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        findViewById<Button>(R.id.btn1).setBackgroundResource(R.drawable.top_bar)
+        findViewById<Button>(R.id.btn1).setTextColor(Color.parseColor("#FFFFFF"))
+        val b1 = findViewById<Button>(R.id.btn1)
+        val b2 = findViewById<Button>(R.id.btn2)
+        val b3 = findViewById<Button>(R.id.btn3)
+
+        btn1.setOnClickListener {
+            b1.setBackgroundResource(R.drawable.top_bar)
+            b2.setBackgroundResource(R.drawable.edit_text_background)
+            b3.setBackgroundResource(R.drawable.edit_text_background)
+            b1.setTextColor(Color.parseColor("#FFFFFF"))
+            b2.setTextColor(Color.parseColor("#80FFFFFF"))
+            b3.setTextColor(Color.parseColor("#80FFFFFF"))
+        }
+        btn2.setOnClickListener {
+            b1.setBackgroundResource(R.drawable.edit_text_background)
+            b2.setBackgroundResource(R.drawable.gender_on_click)
+            b3.setBackgroundResource(R.drawable.edit_text_background)
+            b2.setTextColor(Color.parseColor("#FFFFFF"))
+            b1.setTextColor(Color.parseColor("#80FFFFFF"))
+            b3.setTextColor(Color.parseColor("#80FFFFFF"))
+        }
+        btn3.setOnClickListener {
+            b1.setBackgroundResource(R.drawable.edit_text_background)
+            b2.setBackgroundResource(R.drawable.edit_text_background)
+            b3.setBackgroundResource(R.drawable.top_bar2)
+            b3.setTextColor(Color.parseColor("#FFFFFF"))
+            b1.setTextColor(Color.parseColor("#80FFFFFF"))
+            b2.setTextColor(Color.parseColor("#80FFFFFF"))
+        }
     }
 
     /**
